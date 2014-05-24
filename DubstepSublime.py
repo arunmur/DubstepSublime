262c6e31048bbe57e2a350e4bfbdfc7f3e32e6da
Arun Murali <arun@regentmarkets.com>
2014-05-24 13:13:58 +0800

    Adds mechanism to add custom placehoders in settings file
    - Seperates out DubstepCommand to a seperate class

--- DubstepSublime.py
+++ DubstepSublime.py
@@ -23,7 +23,7 @@ class DubstepRunCommand(sublime_plugin.TextCommand):
 		self.name = name
 
 		if commands is not None:
-			th = DubstepRunThread(commands=commands, on_failure=self.run_failed, on_success=self.run_success)
+			th = DubstepRunThread(commands=commands, filename=self.view.file_name(), on_failure=self.run_failed, on_success=self.run_success)
 			th.start()
 
 	def run_failed(self, message):
@@ -47,25 +47,17 @@ class DubstepRunCommand(sublime_plugin.TextCommand):
 			sublime.message_dialog(message);
 
 
-
 class DubstepRunThread(threading.Thread):
 	def __init__(self, commands=None, filename=None, on_success=None, on_failure=None):
 		self.commands = commands
 		self.on_success = on_success
 		self.on_failure = on_failure
-		self.settings = sublime.load_settings('Dubstep.sublime-settings').get('remote')
-		self.git_branch_placeholder_re = re.compile('##GIT_BRANCH##')
-		self.current_file_placeholder_re = re.compile('##FILE##')
 		self.filename = filename
 		threading.Thread.__init__(self)
 
 	def run(self):
 		if self.commands is None or not isinstance(self.commands, list):
 			sublime.error_message("No command given to run")
-			return
-
-		if not 'host' in self.settings:
-			sublime.error_message("Error: DubstepSublime ssh host isn't configured.")
 			return
 
 		out = tempfile.NamedTemporaryFile(delete=True)
@@ -75,67 +67,98 @@ class DubstepRunThread(threading.Thread):
 				if 'description' in command:
 					sublime.status_message("Running : " + command['description'])
 
-				if  'type' not in command:
-					sublime.error_message("type for command not priovided")
-					return
-
-				if command['type'] == 'ssh':
-					if  'run' not in command:
-						sublime.error_message("run parameter for command not provided")
-						return
-
-					status = call(self._ssh_command(command['run']), shell=True, stderr=err, stdout=out)
-				elif command['type'] == 'scp':
-					if  'file' not in command:
-						sublime.error_message("file parameter for command not provided")
-						return
-
-					status = call(self._scp_command(command['file']), shell=True, stderr=err, stdout=out)
-				else:
-					sublime.error_message("Command type " + command['type'] + " is not known")
-					return
-
+				sublime.message_dialog(DubstepCommand(filename=self.filename).resolve_command(command))
+				status = call(DubstepCommand(filename=self.filename).resolve_command(command), shell=True, stderr=err, stdout=out)
 				if int(status) != 0 and self.on_failure is not None:
 					err = open(err.name, 'r')
 					self.on_failure(err.read())
 					return
 
-
 			if self.on_success is not None:
 				out = open(out.name, 'r')
 				self.on_success(out.read())
 
-			out.close()
-			err.close()
 		except OSError as e:
 			if self.on_failure is not None:
 				self.on_failure(str(e))
+		except Exception as e:
+			if self.on_failure is not None:
+				self.on_failure(str(e))
+		finally:
+			out.close()
+			err.close()
+
+class DubstepCommand:
+	def __init__(self, filename=None):
+		self.filename = filename
+		settings = sublime.load_settings('Dubstep.sublime-settings')
+		self.remote = settings.get('remote')
+		self.placeholders = settings.get('placeholders')
+		self.file_in_remote_re = re.compile('##FILE_IN_REMOTE##')
+		self.file_re = re.compile('##FILE##')
+
+
+	def resolve_command(self, command):
+		if 'host'  not in self.remote:
+			raise Exception("Error: Dubstep remote host isn't configured.")
+			
+		if  'type' not in command:
+			raise Exception("type for command not priovided")
+
+		if command['type'] == 'ssh':
+			if  'run' not in command:
+				raise Exception("run parameter for command not provided")
+			return self.ssh_command(command['run'])
+
+		elif command['type'] == 'scp':
+			if  'file' not in command:
+				raise Exception("file parameter for command not provided")
+			return self.scp_command(command['file'])
+		
+		raise Exception("Command type " + command['type'] + " is not known")
+
+
+	def ssh_command(self, run):
+		ssh_command = "ssh"
+		if  'port'  in self.remote:
+			ssh_command += " -p " + self.remote['port']
+		ssh_command += " " + self.remote['user'] + '@' + self.remote['host']	
+		return ssh_command + " '" + self.replace_placehoders(run) + "'"
+
+	def scp_command(self, filename):
+		scp_command = "scp"
+		if  'port'  in self.remote:
+			scp_command += " -P " + self.remote['port']
+		return scp_command + " " + filename + " " + self.remote['user'] + '@' + self.remote['host'] + self.replace_placehoders(filename)
 
 	def replace_placehoders(self, command):
-		if(self.git_branch_placeholder_re.match(command)):
-			command  = command.replace('##GIT_BRANCH##', self.get_current_branch())
+		command = command.replace('##FILE##', self.filename)
+		command = command.replace('##FILE_IN_REMOTE##', self.file_in_remote())
 
-		if(self.current_file_placeholder_re.match(command)):
-			current_directory = os.getcwd()
-			filename = os.path.relpath(self.filename, current_directory)
-			repo_dir = this.settings['home']
-			filename = filename.replace("\\", "/")
-			command = command.replace('##FILE##', repo_dir + '/' + filename)
+		for placeholder in self.placeholders:
+			replace = list(placeholder)[0]
+			replace_with = self._get_output_of(placeholder[replace])
+			command = command.replace("##" + replace + "##", replace_with)
 
 		return command
 
-	def get_current_branch(self):
-		return check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+	def _get_output_of(self, command):
+		out = tempfile.NamedTemporaryFile(delete=True)
+		status = call(command, shell=True, stdout=out)
+		if(int(status) == 0):
+			out = open(out.name, 'r')
+			output =  out.read()
+			out.close()
+			return output.rstrip()
 
-	def _ssh_command(self, run):
-		ssh_command = "ssh"
-		if  'port'  in self.settings:
-			ssh_command += " -p " + self.settings['port']
-		ssh_command += " " + self.settings['user'] + '@' + self.settings['host']	
-		return ssh_command + " '" + self.replace_placehoders(run) + "'"
+		return "";
 
-	def _scp_command(self, filename):
-		scp_command = "ssh"
-		if  'port'  in self.settings:
-			scp_command += " -P " + self.settings['port']
-		return scp_command + " " + filename + " " + self.settings['user'] + '@' + self.settings['host'] + self.replace_placehoders(filename) 	
+	def file_in_remote(self):
+		filename = self.filename
+		current_directory = os.getcwd()
+		filename = os.path.relpath(filename, current_directory)
+		repo_dir = self.remote['home']
+		return repo_dir + '/' + filename.replace("\\", "/")
+		
+
+
